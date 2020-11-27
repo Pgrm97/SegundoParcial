d3d64cc4b6ace9535790acf6fac691e1a0ddaed4
Pgrm97 <Pgrm97@gmail.com>
2020-11-26 20:06:55 -0400

    Added update to product

--- app/src/main/java/com/pucmm/segundoparcial/ProductAdapter.java
+++ app/src/main/java/com/pucmm/segundoparcial/ProductAdapter.java
@@ -8,13 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.pucmm.segundoparcial.ui.gallery.GalleryFragment;
+import com.pucmm.segundoparcial.ui.slideshow.SlideshowFragment;
 
 import java.util.List;
 
@@ -23,6 +29,9 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
     private List<ProductData> dataList;
     private Activity context;
     private RoomDB database;
+
+    Spinner categorySpinner;
+    private List<String> dataListSpinner;
 
     //Create constructor
     public ProductAdapter(Activity context, List<ProductData> dataList){
@@ -58,49 +67,70 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
             @Override
             public void onClick(View v) {
 
-//                //Initialize main data
-//                MainData d = dataList.get(holder.getAdapterPosition());
-//                //Get id
-//                final int sID = d.getID();
-//                // Get Text
-//                String sText = d.getText();
-//
-//                //Create dialog
-//                final Dialog dialog = new Dialog(context);
-//                //Set content view
-//                dialog.setContentView(R.layout.dialog_update);
-//                //Initialize width
-//                int width = WindowManager.LayoutParams.MATCH_PARENT;
-//                //Initialize height
-//                int height = WindowManager.LayoutParams.WRAP_CONTENT;
-//                //Set layout
-//                dialog.getWindow().setLayout(width,height);
-//                //Show dialog
-//                dialog.show();
-//
-//                //Initialize and assign variable
-//                final EditText editText = dialog.findViewById(R.id.edit_text);
-//                Button btUpdate = dialog.findViewById(R.id.bt_update);
-//
-//                //Set text on edit text
-//                editText.setText(sText);
-//
-//                btUpdate.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        //Dismiss dialog
-//                        dialog.dismiss();
-//
-//                        //Get updated text from edit text
-//                        String uText = editText.getText().toString().trim();
-//                        //Update text in database
-//                        database.mainDao().update(sID,uText);
-//                        //Notify when data is updated
-//                        dataList.clear();
-//                        dataList.addAll(database.mainDao().getAll());
-//                        notifyDataSetChanged();
-//                    }
-//                });
+                //Initialize main data
+                ProductData d = dataList.get(holder.getAdapterPosition());
+                //Get id
+                final int sID = d.getID();
+                // Get Text
+                String sName = d.getName();
+                // Get Amount
+                String sAmount = d.getAmountString();
+                // Get Text
+                String sCategory = d.getCategory();
+
+
+                //Create dialog
+                final Dialog dialog = new Dialog(context);
+                //Set content view
+                dialog.setContentView(R.layout.dialog_update_product);
+                //Initialize width
+                int width = WindowManager.LayoutParams.MATCH_PARENT;
+                //Initialize height
+                int height = WindowManager.LayoutParams.WRAP_CONTENT;
+                //Set layout
+                dialog.getWindow().setLayout(width,height);
+                //Show dialog
+                dialog.show();
+
+                //Store database value in data list
+                dataListSpinner = database.mainDao().getAllCategories();
+
+                categorySpinner = dialog.findViewById(R.id.spinnerUpdateProduct);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item,  dataListSpinner);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                categorySpinner.setAdapter(adapter);
+
+                //Initialize and assign variable
+                final EditText editName = dialog.findViewById(R.id.edit_text_productname);
+                final EditText editAmount = dialog.findViewById(R.id.edit_text_productprice);
+
+                Button btUpdate = dialog.findViewById(R.id.bt_update_product);
+
+                //Set text on edit text
+                editName.setText(sName);
+                editAmount.setText(sAmount);
+
+                btUpdate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Dismiss dialog
+                        dialog.dismiss();
+
+                        //Get updated text from edit text
+                        String uName = editName.getText().toString().trim();
+                        //Get updated text from edit text
+                        int uAmount = Integer.parseInt(editAmount.getText().toString().trim());
+                        //Get updated text from edit text
+                        String uCategory = categorySpinner.getSelectedItem().toString();
+                        //Update text in database
+                        database.productDao().update(sID,uName,uAmount,uCategory);
+                        //Notify when data is updated
+                        dataList.clear();
+                        dataList.addAll(database.productDao().getAll());
+                        notifyDataSetChanged();
+                    }
+                });
             }
         });
 
